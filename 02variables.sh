ea9711dea9a6b724568207662865fdb125304917
Vidyasagar <vidyasagar.nellipudi@gmail.com>
2024-01-16 00:18:57 +0530

    practice

--- 02variables.sh
+++ 02variables.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+echo "ramesh : hello suresh , Good morning"
+echo "Suresh : Hi ramesh, Very Good Morning"
+echo "ramesh : how are you suresh?
+echo "suresh : i am good ramesh how are you"
