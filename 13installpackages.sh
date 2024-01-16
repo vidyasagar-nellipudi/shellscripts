f4ecaf06646fdf5449012e416fd00a8a2780d597
Vidyasagar <vidyasagar.nellipudi@gmail.com>
2024-01-17 02:09:38 +0530

    practice

--- 13installpackages.sh
+++ 13installpackages.sh
@@ -2,7 +2,14 @@
 
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOG="/tmp/$0-$TIMESTAMP.log"
+
+echo " script started executing at $TIMESTAMP" &>> $LOG
 
 # Checking root user or not
 
@@ -16,5 +23,31 @@ else
     echo -e "$G you are root user $N"
 fi
 
-echo " All arguments passed: $@"
+#echo " All arguments passed: $@"
+
+# VALIDATE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $2 .... $R Failed $N"
+    else
+        echo -e " $2 .... $G Success $N"
+    fi
+}
+
+for package in $@
+do
+    yum list installed $package &>> $LOG
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOG
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e " $package is already installed....$Y skipping $N"
+    fi
+
+done
+
+
 
